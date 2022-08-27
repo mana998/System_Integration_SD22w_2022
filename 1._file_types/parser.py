99ed0525100970495e1d46a899e73ee807c11cc7
Marianna <mana998@azet.sk>
2022-08-27 17:11:31 +0200

    Added xml parsing

--- 1._file_types/parser.py
+++ 1._file_types/parser.py
@@ -1,5 +1,6 @@
 import pandas
 import json
+import xml.etree.ElementTree as ET
 
 #parse csv
 csv = pandas.read_csv('./movie.csv')
@@ -9,3 +10,9 @@ print('csv\n', csv)
 with open('./movie.json', 'r') as f:
   data = json.load(f)
 print('json\n', data)
+
+#parse xml
+root = ET.parse('./movie.xml').getroot()
+print('xml\n')
+for element in root:
+     print(element.tag, element.text)

34b11fd63319b733ef5cfe2c7295eb8ecb1249c9
Marianna <mana998@azet.sk>
2022-08-27 17:11:09 +0200

    Added json parsing

--- 1._file_types/parser.py
+++ 1._file_types/parser.py
@@ -1,6 +1,11 @@
 import pandas
-
+import json
 
 #parse csv
 csv = pandas.read_csv('./movie.csv')
 print('csv\n', csv)
+
+#parse json
+with open('./movie.json', 'r') as f:
+  data = json.load(f)
+print('json\n', data)

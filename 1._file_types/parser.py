c019b3664fa2a2032e7f6635382d7e63ab6d1b24
Marianna <mana998@azet.sk>
2022-08-27 17:11:43 +0200

    Added yaml parsing

--- 1._file_types/parser.py
+++ 1._file_types/parser.py
@@ -1,6 +1,7 @@
 import pandas
 import json
 import xml.etree.ElementTree as ET
+import yaml
 
 #parse csv
 csv = pandas.read_csv('./movie.csv')
@@ -16,3 +17,11 @@ root = ET.parse('./movie.xml').getroot()
 print('xml\n')
 for element in root:
      print(element.tag, element.text)
+
+#parse yaml
+print('yaml\n')
+with open("./movie.yml", "r") as stream:
+    try:
+        print(yaml.safe_load(stream))
+    except yaml.YAMLError as exc:
+        print(exc)
